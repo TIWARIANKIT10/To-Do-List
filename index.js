8482bdb5e55fed25994ab92fafcd43476ade46f9
TIWARIANKIT10 <00tiwariankit00@gmail.com>
2025-03-09 18:29:57 +0545

    all element selected in js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // select item
+const alert = document.getElementsByClassName('alert');
+const addlist = document.getElementById('addlist');
+const submit_button = document.getElementsByClassName('submit-button');
+const  edit  = document.getElementsByClassName('edit-btn'); 
+const deletes = document.getElementsByClassName('delete-btn');
+const clear = document.getElementsByClassName('clear');
+const form = document.querySelector('.todolistform')
+
+
+//edit option 
+let editElement ;
+let editFlag = false ;
+let editID = "";
+
+// event listening 
+form.addEventListener("submit", additem);
+function additem(e){
+    e.preventDefault();
+    console.log('a')
+  
+}
+
+
+//function 
+
+
+
+
+
