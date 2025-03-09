dc47a778f941d62c08bdcbbbc9f372dfdcd42716
TIWARIANKIT10 <00tiwariankit00@gmail.com>
2025-03-09 20:38:35 +0545

    data add option added

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // select item
-const alert = document.getElementsByClassName('alert');
+const alert = document.getElementById('alert');
 const addlist = document.getElementById('addlist');
 const submit_button = document.getElementsByClassName('submit-button');
 const  edit  = document.getElementsByClassName('edit-btn'); 
 const deletes = document.getElementsByClassName('delete-btn');
 const clear = document.getElementsByClassName('clear');
 const form = document.querySelector('.todolistform')
+const list = document.getElementsByClassName('todo-list');
 
 
 //edit option 
@@ -17,7 +18,49 @@ let editID = "";
 form.addEventListener("submit", additem);
 function additem(e){
     e.preventDefault();
-    console.log('a')
+    const value = addlist.value;
+    const id = new Date().getTime().toString();
+
+    if(value && editFlag=== false){
+        
+        const todo_item = document.createElement('article');
+        todo_item.setAttribute('data-id', id);
+        todo_item.innerHTML =`<p class="title">item</p>
+                <div class="btn-container">
+                    <button type="button" class="edit-btn">
+                        <i class="fas fa-edit">edit</i>
+                    </button>
+                    <button type="button" class="delete-btn">
+                        <i class="fas fa-delete">delete</i>`;
+                        list.appendChild(todo_item);
+            
+   
+        
+   alerts("items added","success");
+    }
+
+    else if(value && editFlag===true){
+     console.log("value is editing here");
+     alerts("items edited","success");
+
+    }
+    else{
+        alerts("enter some value","danger");
+
+    }
+
+    //function
+    function alerts(text,action){
+        alert.textContent = text ;
+        alert.classList.add(`alert-${action}`);
+
+        // settimeout 
+        setTimeout(function(){
+            alert.textContent="";
+            alert.classList.tooggle(`aler-${action}`);
+        },6000)
+    }
+
   
 }
 
