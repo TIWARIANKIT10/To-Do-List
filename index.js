c685e66309c0238dfdffca64db0c69f1108e3938
TIWARIANKIT10 <00tiwariankit00@gmail.com>
2025-03-10 10:16:30 +0545

    function added to clear delete  btn

--- index.js
+++ index.js
@@ -2,11 +2,9 @@
 const alert = document.getElementById('alert');
 const addlist = document.getElementById('addlist');
 const submit_button = document.getElementsByClassName('submit-button');
-const  edit  = document.getElementsByClassName('edit-btn'); 
-const deletes = document.getElementsByClassName('delete-btn');
-const clear = document.getElementsByClassName('clear');
+const clear = document.getElementById('clear');
 const form = document.querySelector('.todolistform')
-const list = document.getElementsByClassName('todo-list');
+const list = document.querySelector('.todo-list');
 
 
 //edit option 
@@ -16,6 +14,14 @@ let editID = "";
 
 // event listening 
 form.addEventListener("submit", additem);
+
+//clear 
+clear.addEventListener("click", function(){
+    const remove = document.querySelectorAll('.todo-item');
+    remove.forEach(function(items){
+        list.removeChild(items);
+    });
+});
 function additem(e){
     e.preventDefault();
     const value = addlist.value;
@@ -24,23 +30,44 @@ function additem(e){
     if(value && editFlag=== false){
         
         const todo_item = document.createElement('article');
-        todo_item.setAttribute('data-id', id);
-        todo_item.innerHTML =`<p class="title">item</p>
+        todo_item.classList.add('todo-item');
+        todo_item.setAttribute('id', id);
+        todo_item.innerHTML =`<p class="title">${addlist.value}</p>
                 <div class="btn-container">
                     <button type="button" class="edit-btn">
                         <i class="fas fa-edit">edit</i>
                     </button>
                     <button type="button" class="delete-btn">
                         <i class="fas fa-delete">delete</i>`;
-                        list.appendChild(todo_item);
+                      
+
+   const  edit  = todo_item.querySelector('.edit-btn'); 
+const deletes = todo_item.querySelector('.delete-btn');
+
+edit.addEventListener("click", editfunction); 
+deletes.addEventListener('click',deletefunction);
+
+//append child
+list.appendChild(todo_item);
+
+                        //add to local storage 
+
+
+                        //set back to default
+
             
    
         
    alerts("items added","success");
-    }
+   setBackToDefault();
 
+    }
+    
     else if(value && editFlag===true){
-     console.log("value is editing here");
+
+
+     
+        
      alerts("items edited","success");
 
     }
@@ -53,12 +80,13 @@ function additem(e){
     function alerts(text,action){
         alert.textContent = text ;
         alert.classList.add(`alert-${action}`);
+        console.log(alert.getAttribute('class'));
 
         // settimeout 
         setTimeout(function(){
             alert.textContent="";
-            alert.classList.tooggle(`aler-${action}`);
-        },6000)
+            alert.classList.toggle(`alert-${action}`);
+        },60);
     }
 
   
@@ -66,6 +94,44 @@ function additem(e){
 
 
 //function 
+
+
+//local storage 
+ function addToLocalStorage(id,value){
+
+}
+
+function removeFromLocalStorage(){
+    
+}
+
+
+// set  back to default 
+function setBackToDefault(){
+    addlist.value="";
+    editID = "";
+
+    
+
+}
+
+
+//edit 
+function editfunction(){
+console.log("item edited");
+}
+
+function deletefunction(e){
+ const element = e.currentTarget.parentElement.parentElement;
+ list.removeChild(element);
+   
+}
+
+
+
+
+
+
 
 
 
